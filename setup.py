da558305d63ea3129ec89f61e4ff75a8f0e3ad83
RishabhVerma <r.verma@sigmoidanalytics.com>
2023-11-06 14:10:43 +0530

    bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sigmoidpythonpackage",
-    version="0.2",
+    version="0.3.0",
     packages=find_packages(),
     install_requires=["pandas"],
 )
