3ae1fb689916a230f298a296c413644fa62195a8
rishabhvermarv99 <138434011+rishabhvermarv99@users.noreply.github.com>
2023-11-06 12:01:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sigmoidpythonpackage",
-    version="{version}",
+    version="0.1",
     packages=find_packages(),
     install_requires=["pandas"],
 )
