7d2c615d1b8431f577805e9bb88a7d6b84328644
rishabhvermarv99 <rishabhverma3012.rv@gmail.com>
2023-11-07 17:58:32 +0530

    patch: made some small changes

--- .github/workflows/check_package_version.sh
+++ .github/workflows/check_package_version.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Define the package name you want to check
-package_name="django"
+# # Define the package name you want to check
+# package_name="django"
 
-# Use pip to fetch the package information
-package_info=$(pip show $package_name)
-echo "$package_info"
-# Extract and display the package version
-package_version=$(echo "$package_info")
-# echo "Package Version: $package_version"
-curl -s "https://pypi.org/pypi/sigmoidpythonpackage/json" | jq -r '.info.version'
+# # Use pip to fetch the package information
+# package_info=$(pip show $package_name)
+# echo "$package_info"
+# # Extract and display the package version
+# package_version=$(echo "$package_info")
+# # echo "Package Version: $package_version"
+# curl -s "https://pypi.org/pypi/sigmoidpythonpackage/json" | jq -r '.info.version'
